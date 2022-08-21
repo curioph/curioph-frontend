55c8464d6e588dd57adeb24ea6fd7cfbd7b1be76
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2022-08-21 13:53:16 +0900

    :tada: Initialize Curioph Frontend project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: 'Curioph',
+  meta: [
+    { name: 'description', content: 'curiophとは、現実世界の「興味/関心のある行動」と連動し、Web上にコミュニティが出現するサービスです。' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
